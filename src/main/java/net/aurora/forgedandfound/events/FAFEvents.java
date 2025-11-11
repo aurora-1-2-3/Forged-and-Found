38a1b9ce3e420454494734c16293f63add19ed3d
Aurora <139906198+aurora-1-2-3@users.noreply.github.com>
2025-11-11 17:35:02 +0500

    shhhhh
    you didnt see anything

--- src/main/java/net/aurora/forgedandfound/events/FAFEvents.java
+++ src/main/java/net/aurora/forgedandfound/events/FAFEvents.java
@@ -1,12 +1,8 @@
 package net.aurora.forgedandfound.events;
 
 
-import net.aurora.forgedandfound.ForgedAndFound;
-import net.neoforged.fml.common.EventBusSubscriber;
-
-
-@SuppressWarnings("removal")
-@EventBusSubscriber(modid = ForgedAndFound.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
+/*@SuppressWarnings("removal")
+@EventBusSubscriber(modid = ForgedAndFound.MOD_ID, bus = EventBusSubscriber.Bus.GAME)*/
 public class FAFEvents {
-
 }
+
